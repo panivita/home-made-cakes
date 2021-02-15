3b33539f2dd65beab2498bfaed8c581de0cdae75
VictoriaKush <victoria.v.kush@gmail.com>
2021-02-15 09:49:15 +0100

    install project

---
+++ src/frontend/build/precache-manifest.f5387b87513f8c85ee8cfb41b177d4c8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6894300aa40313eeb652fcfdd54c385d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4f90ef165d7f0c9418",
+    "url": "/static/css/2.a00eef2d.chunk.css"
+  },
+  {
+    "revision": "d51126439cff06e39a55",
+    "url": "/static/css/main.babf7a7f.chunk.css"
+  },
+  {
+    "revision": "6f4f90ef165d7f0c9418",
+    "url": "/static/js/2.814ae0a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.814ae0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51126439cff06e39a55",
+    "url": "/static/js/main.ea7dec94.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "e14cfbf3b50df9197a0b4b5c6ebd15c4",
+    "url": "/static/media/background.e14cfbf3.jpg"
+  },
+  {
+    "revision": "a58ea15d8b214f6a839c85051d0bd9e4",
+    "url": "/static/media/cakes.a58ea15d.png"
+  },
+  {
+    "revision": "602aa2ce3e96630d8b78628b75156fe7",
+    "url": "/static/media/cupcake.602aa2ce.png"
+  },
+  {
+    "revision": "cdeae3796e0ded39cbf3664083adf2c7",
+    "url": "/static/media/homemade-logo.cdeae379.png"
+  },
+  {
+    "revision": "c588a0aa52e5f862ae6b9c8b3bb80357",
+    "url": "/static/media/logo.c588a0aa.png"
+  }
+]);
